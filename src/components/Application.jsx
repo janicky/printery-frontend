c8e1d9da2c9890edaa9dda101889998ced396bbc
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 11:28:37 +0100

    Add react router dom library

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 export default class Application extends Component {
   render() {
     return (
-      <div>
-        ...application...
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={null} />
+      </BrowserRouter>
     )
   }
 }
