20745a256838f094c2c97dc18bd07115fb90d293
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 15:57:00 +0100

    Add loaded condition to protected layout

--- src/components/layouts/ProtectedLayout.jsx
+++ src/components/layouts/ProtectedLayout.jsx
@@ -4,8 +4,23 @@ import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
 export class ProtectedLayout extends Component {
+
+  state = {
+    loaded: false
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user.loaded !== this.state.loaded) {
+      this.setState({ loaded: nextProps.user.loaded })
+    }
+  }
+
   render() {
-    const { user } = this.props
+    const { loaded } = this.state
+    const { user} = this.props
+    if (!loaded) {
+      return <div></div>
+    }
     if (!user.logged) {
       return <Redirect to="/login" />
     }
