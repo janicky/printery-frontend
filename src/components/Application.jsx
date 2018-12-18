dcd3f5274462eae30f6414b9eb72c7bccf03106d
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 11:38:08 +0100

    Add layouts implementation

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+// Import layouts
+import GuestLayout from './layouts/GuestLayout'
+
 export default class Application extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={null} />
+        <GuestLayout exact path="/" component={null} />
       </BrowserRouter>
     )
   }
