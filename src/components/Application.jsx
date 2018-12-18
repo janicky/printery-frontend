c03aa9836a09fe8863154e1e1da1a2c7fd74ab67
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 11:39:38 +0100

    Add home page route

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 // Import layouts
 import GuestLayout from './layouts/GuestLayout'
+
+// Import pages
+import Home from './pages/Home'
 
 export default class Application extends Component {
   render() {
     return (
       <BrowserRouter>
-        <GuestLayout exact path="/" component={null} />
+        <GuestLayout exact path="/" component={Home} />
       </BrowserRouter>
     )
   }
