1a24bb0844d33c4849a191bb1d2acebe5e6d7e96
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 12:34:44 +0100

    Add sign in form

--- src/components/pages/SignIn/SignIn.jsx
+++ src/components/pages/SignIn/SignIn.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+
 export class SignIn extends Component {
   render() {
     return (
-      <div className="d-flex p-2 justify-content-center align-items-center h-100">
-        ...test...
+      <div className="d-flex p-2 justify-content-center align-items-center h-100 bg-light">
+        <div className="p-3 border rounded shadow-sm bg-white">
+          <Form>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="Wprowadź adres email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Hasło</Label>
+              <Input type="password" name="password" id="password" placeholder="Wprowadź hasło" />
+            </FormGroup>
+            <div className="d-flex justify-content-center">
+              <Button>Zaloguj się</Button>
+            </div>
+          </Form>
+        </div>
       </div>
     )
   }
