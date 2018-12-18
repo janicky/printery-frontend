8ca7c8c628cc38f426918ceeee189294128ac421
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 11:03:21 +0100

    Add bootstrap to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './components/Application';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Application from './components/Application'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(<Application />, document.getElementById('root'));
 
