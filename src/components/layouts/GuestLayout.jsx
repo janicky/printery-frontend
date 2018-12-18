88c555f6d2ac92d46832703da1a62f61e0948dcf
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 12:42:38 +0100

    Add  guest layout header

--- src/components/layouts/GuestLayout.jsx
+++ src/components/layouts/GuestLayout.jsx
@@ -4,7 +4,10 @@ import { Route } from 'react-router-dom'
 export default class GuestLayout extends Component {
   render() {
     return (
-      <Route {...this.props} />
+      <div className="h-100 d-flex p-2 justify-content-center align-items-center bg-light flex-column">
+        <div className="display-4 mb-4 text-primary">printery</div>
+        <Route {...this.props} />
+      </div>
     )
   }
 }
