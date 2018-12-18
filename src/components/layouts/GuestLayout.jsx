540eb18dabea929be5822cc997222a5a93977d5b
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-18 23:31:21 +0100

    Add logo to guest layout

--- src/components/layouts/GuestLayout.jsx
+++ src/components/layouts/GuestLayout.jsx
@@ -6,6 +6,9 @@ import { Route, Redirect, withRouter } from 'react-router-dom'
 // Import modules
 import FullscreenLoading from '../modules/FullscreenLoading'
 
+// Import logo
+import logo from '../../images/logo_light.png'
+
 export class GuestLayout extends Component {
 
   state = {
@@ -36,7 +39,9 @@ export class GuestLayout extends Component {
 
     return (
       <div className="h-100 d-flex p-2 justify-content-center align-items-center bg-light flex-column">
-        <div className="display-4 mb-4 text-primary">printery</div>
+        <div className="p-5">
+          <img src={logo} alt="Logo" className="img-responsive" width="300"/>
+        </div>
         <Route {...this.props} />
       </div>
     )
