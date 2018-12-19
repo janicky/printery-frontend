fedf25bb00b8cc3a882b321dc3c793af50f6e3cf
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-19 17:17:23 +0100

    Add data to company row

--- src/components/modules/Companies/CompanyRow.jsx
+++ src/components/modules/Companies/CompanyRow.jsx
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 export default class CompanyRow extends Component {
   render() {
+    const { id, name, address } = this.props
     return (
       <tr>
-        <td></td>
+        <td>{id}</td>
+        <td>{name}</td>
+        <td>{address}</td>
       </tr>
     )
   }
