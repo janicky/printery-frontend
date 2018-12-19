d9d0a611a812cd37cac2d5f5f3dd7191b6a8d549
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-19 17:35:24 +0100

    Add buttons to company row

--- src/components/modules/Companies/CompanyRow.jsx
+++ src/components/modules/Companies/CompanyRow.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
+
+import { Button } from 'reactstrap'
 
 export default class CompanyRow extends Component {
   render() {
@@ -8,6 +12,14 @@ export default class CompanyRow extends Component {
         <td>{id}</td>
         <td>{name}</td>
         <td>{address}</td>
+        <td className="text-center">
+          <Link to={`/companies/${id}`} className="btn btn-sm btn-dark m-1">
+            <FontAwesomeIcon icon={['fas', 'search']} />
+          </Link>
+          <Button size="sm" className="m-1" color="danger" onClick={() => this.handleDelete(id)}>
+            <FontAwesomeIcon icon={['fas', 'trash-alt']} />
+          </Button>
+        </td>
       </tr>
     )
   }
