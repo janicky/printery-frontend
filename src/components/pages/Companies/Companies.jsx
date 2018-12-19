e5aef112553e55c58308b2f93945811bcf6d93ed
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-19 17:02:44 +0100

    Add request to companies component

--- src/components/pages/Companies/Companies.jsx
+++ src/components/pages/Companies/Companies.jsx
@@ -1,26 +1,45 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { request } from '../../../request'
+
+// Import redux actions
+import { setCompanies, deleteCompany } from '../../../actions/companies'
+import EmptyState from '../../modules/EmptyState';
+
 class Companies extends Component {
   render() {
-    return <div></div>
+    const { companies } = this.props
+
+    if (companies.length === 0) {
+      return <EmptyState icon={['fas', 'users']} title="Brak klientów" message="Ale jeszcze może będą..." />
+    }
+
+    return <div>{companies.map(company => <div>{company.name}</div>)}</div>
   }
 }
 
 export class CompaniesContainer extends Component {
+
+  componentDidMount() {
+    request.get('companies')
+      .then(response => this.props.setCompanies(response.data))
+  }
+
   render() {
+    const { companies } = this.props
     return (
-      <Companies />
+      <Companies companies={companies} />
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  
+  companies: state.companies
 })
 
 const mapDispatchToProps = {
-  
+  setCompanies, deleteCompany
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CompaniesContainer)
