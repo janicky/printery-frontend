07e4db51b17a6f28b40a09a96502e24847ea24e0
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-19 11:58:01 +0100

    Add user row component

---
+++ src/components/modules/Users/UserRow.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserRow extends Component {
+  render() {
+    return (
+      <div>
+        to jest wiersz
+      </div>
+    )
+  }
+}
