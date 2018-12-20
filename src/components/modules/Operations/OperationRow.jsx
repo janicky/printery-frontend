b27a32a7c7bf019d8173dec377a0fb9302af88e5
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-20 19:14:05 +0100

    Initialize operation row component

---
+++ src/components/modules/Operations/OperationRow.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OperationRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>...</td>
+      </tr>
+    )
+  }
+}
